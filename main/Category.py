dba132a89ccdcf532089a22018a943d94cca4f35
Ksenia-Kostyuk <hatan2235@mail.ru>
2024-03-06 19:25:45 +0300

    lhg

---
+++ main/Category.py
@@ -0,0 +1,31 @@
+from main.Products import Product
+
+
+class Category:
+    """Описание категории товаров"""
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.__products = products
+
+    def __len__(self):
+        return len(self.__products)
+
+    def __str__(self):
+        return f'{self.name}, количество продуктов: {len(self.__products)}'
+
+    def get_products(self):
+        result = ''
+        for product in self.__products:
+            result += f'{product.name}, {product.price}, остаток {product.remain}'
+        return result
+
+    @property
+    def products(self):
+        return self.__products
+
+    @products.setter
+    def products(self, new_product):
+        get_object = Product(name=new_product['name'], description=new_product['description'], price=new_product['price'], quantity=new_product['quantity'])
+        self.__products.append(get_object)
